2a831f0e3e08b979adcbe0b4087046359b7050c1
Anthony Bertrand <anthony-bertrand@live.fr>
2019-10-19 11:54:39 +0200

    Cholesky decomposition implemented, should test for matrix > 3x3

--- cholesky.c
+++ cholesky.c
@@ -4,7 +4,7 @@
 #include "testMatrice.h"
 
 
-void decomp_cholesky(float *tab, float *decomp, int N){
+/*void decomp_cholesky(float *tab, float *decomp, int N){
     int i, j;
     for(i = 0; i < N; i++){
         decomp[i*N + i] = sqrt(tab[i*N + i]);
@@ -14,9 +14,36 @@ void decomp_cholesky(float *tab, float *decomp, int N){
         }
         
     }
+}*/
+
+
+int decomp_cholesky(float *tab, float *decomp, int N){
+    int i, j, s, k;
+    float somme;
+    for(i = 0; i < N; i++){
+        somme = 0;
+        for(j = 0; j <= i-1; j++){
+            somme += pow(decomp[j*N + i], 2);
+        }
+        printf("tab[i][i] = %f\n", tab[i*N + i]);
+        printf("somme1 = %f\n", somme);
+        s = tab[i*N + i] - somme;
+        if(s <= 0){
+            printf("erreur cholesky : matrice non definie positive\n");
+            return -1;
+        }
+        decomp[i*N + i] = sqrt(s);
+        for(j = i+1; j < N; j++){
+            somme = 0;
+            for (k = 0; k <= i-1; k++)
+            {
+                somme += decomp[k*N + i] * decomp[k*N + j];
+            }
+            decomp[i*N + j] = (tab[i*N + j] - somme) / decomp[i*N + i];
+        }
+    }
+
 }
-
-
 
 
 
--- cholesky.h
+++ cholesky.h
@@ -1,7 +1,7 @@
 #ifndef H_CHOLES
 #define H_CHOLES
 
-void decomp_cholesky(float *tab, float decomp, int N);
+int decomp_cholesky(float *tab, float decomp, int N);
 int cholesky(float *tab, float *res, int N);
 
 #endif
